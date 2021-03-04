0377b25c26b486bfac7a7e154e74794bf66b6271
FeralDaro <meral.nekrag@gmail.com>
2021-03-04 22:55:56 +0300

    feat: add function tower-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if(matrix === undefined){
+        return []
+    }
+    let arr = [];
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                arr.push(matrix[i][j])
+            }
+        }
+        if (i % 2 === 1) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                arr.push(matrix[i][matrix[i].length - 1 - j])
+            }
+        }
+    }
+    return arr;
 }
